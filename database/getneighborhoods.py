a181c275ea8b4abab9a3feffcdbe7fa37a0501c0
guilhermesdas <guilhermesdas@gmail.com>
2019-06-28 18:47:34 -0400

    Keywords and seeds putted in database

---
+++ database/getneighborhoods.py
@@ -0,0 +1,15 @@
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import json
+
+url = 'https://places-dsn.algolia.net/1/places/query'
+post = {}
+post["@query"] = "Manaus ufam"
+header = {
+"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+"X-Requested-With": "XMLHttpRequest"
+}
+
+request = Request(url, urlencode(post).encode())
+json = urlopen(request).read().decode()
+print(json)
