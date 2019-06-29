44f522ffaa663c0643fce6c556269bb14ad5de82
guilhermesdas <guilhermesdas@gmail.com>
2019-06-29 10:49:45 -0400

    Beginning of javaapi

---
+++ javaapi/newssites/Requests.java
@@ -0,0 +1,106 @@
+package newssites;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.http.HttpClient;
+
+import javax.net.ssl.HttpsURLConnection;
+import org.json.simple.JSONObject;
+
+public class Requests {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		Requests http = new Requests();
+
+		//System.out.println("Testing 1 - Send Http GET request");
+		//http.sendGet();
+		
+		JSONObject obj = new JSONObject();
+
+	     obj.put("keyword","123");
+		
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost("http://localhost:3000/keywords/add",obj.toString());
+		
+	}
+	
+	// HTTP POST request
+	private void sendPost(String url, String params) throws Exception {
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		//add request header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+		
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(params);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + params);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		//print result
+		System.out.println(response.toString());
+
+	}
+	
+
+
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "http://www.google.com/search?q=mkyong";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+
+	}
+
+}
